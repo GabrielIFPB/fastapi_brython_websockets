7a6d6931538d4256c82b468ba509610b677fd37c
GabrielIFPB <jeronimogbariel@gmail.com>
2021-04-27 18:09:45 -0300

    creating static template

---
+++ app/static.py
@@ -0,0 +1,15 @@
+
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+static_router = APIRouter()
+templates = Jinja2Templates(directory='templates')
+
+
+@static_router.get('/template', response_class=HTMLResponse)
+def route(request: Request):
+	return templates.TemplateResponse(
+		'static.html',
+		{'request': request}
+	)
--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from app.static import static_router
+
+app = FastAPI()
+
+app.mount(
+    '/static',
+    StaticFiles(directory='static'),
+    name='static'
+)
+
+
+@app.get("/")
+def root():
+    a = "a"
+    b = "b" + a
+    return {"hello world": b}
+
+
+@app.get("/sum/{num1}/{num2}")
+def somar(num1: int, num2: int):
+    return {"sum": num1 + num2}
+
+
+app.include_router(static_router)
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
